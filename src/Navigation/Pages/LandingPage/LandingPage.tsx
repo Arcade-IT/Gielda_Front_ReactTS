254664029e8042ed9e50c2f7d89e99d09189a4d8
KarolP1 <52258025+KarolP1@users.noreply.github.com>
2020-09-23 00:06:43 +0200

    KP_styles: working at LandingPage first try( box )

--- src/Navigation/Pages/LandingPage/LandingPage.tsx
+++ src/Navigation/Pages/LandingPage/LandingPage.tsx
@@ -1,13 +1,52 @@
 import React from 'react';
-import { Wrapper } from './Landingpage.styled';
+import {
+    Wrapper,
+    Main,
+    ViewWraper,
+    TopSection,
+    LogoContainer,
+    ButtonContainer,
+    CustomButton,
+    ContentContainer,
+    ContentLeft,
+    ContentRight,
+    TitleAppContainer,
+} from './Landingpage.styled';
 import { ThemeProvider } from 'styled-components';
-import { colorTheme, shadowTheme } from '../../../themes';
+import { colorTheme, shadowTheme, fontTheme } from '../../../themes';
 
 const Landingpage: React.FC = (props) => {
     return (
         <ThemeProvider theme={colorTheme}>
             <Wrapper>
-                <ThemeProvider theme={shadowTheme}>Landingpage</ThemeProvider>
+                <ThemeProvider theme={shadowTheme}>
+                    <ThemeProvider theme={fontTheme}>
+                        <Main>
+                            <ViewWraper>
+                                <TopSection>
+                                    <LogoContainer>Logo</LogoContainer>
+                                    <ButtonContainer>
+                                        <CustomButton>Home</CustomButton>
+                                        <CustomButton>About us</CustomButton>
+                                        <CustomButton>Services</CustomButton>
+                                        <CustomButton>App</CustomButton>
+                                        <CustomButton>Contact</CustomButton>
+                                        <CustomButton>More Info</CustomButton>
+                                    </ButtonContainer>
+                                </TopSection>
+                                <ContentContainer>
+                                    <ContentLeft>
+                                        <TitleAppContainer>
+                                            <p>Forex</p>
+                                            <p>Arcade IT</p>
+                                        </TitleAppContainer>
+                                    </ContentLeft>
+                                    <ContentRight>hello</ContentRight>
+                                </ContentContainer>
+                            </ViewWraper>
+                        </Main>
+                    </ThemeProvider>
+                </ThemeProvider>
             </Wrapper>
         </ThemeProvider>
     );
--- src/themes/colorTheme.ts
+++ src/themes/colorTheme.ts
@@ -2,6 +2,7 @@ export const colorTheme = {
     primaryDark: '#1a1c20',
     primaryMedium: '#40454f',
     primaryLight: '#4f5562',
+    primaryLighter: '#6f7584',
     primaryLightest: '#f4f4f4',
     secondaryDark: '#cf7500',
     secondaryMedium: '#ff9103',
